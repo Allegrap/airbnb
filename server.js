7b99081f09efb2f03a468e5ce494c996bbbd17c9
Allegra <allegra.perazzo@googlemail.com>
2017-08-22 13:33:55 +0100

    setup

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var cheerio = require('cheerio');
+var request = require('request');
+var fs = require('fs');
+var app = express();
+
+
+
+
+
+app.listen('3000')
+console.log('Listening on port 3000');
+module.exports = app;
