208e4a8974e4b27a5a3f60b70cf49d842d1ccac5
Allegra <allegra.perazzo@googlemail.com>
2017-08-22 14:03:26 +0100

    name request for first url set up

--- server.js
+++ server.js
@@ -4,10 +4,45 @@ var request = require('request');
 var fs = require('fs');
 var app = express();
 
+app.get('/airbnb', function(req, res){
 
+  url = 'https://www.airbnb.co.uk/rooms/14531512?s=51';
+
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
+
+      var name;
+      var json = { name : "" };
+
+      $('.listing_name').filter(function(){
+          var data = $(this);
+          name = data.text();
+
+          json.name = name;
+      })
+    }
+
+    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+              console.log('Success!');
+          })
+
+    res.send('Should be done.')
+
+  })
+
+
+
+
+
+})
 
 
 
 app.listen('3000')
 console.log('Listening on port 3000');
 module.exports = app;
+
+
+
+
